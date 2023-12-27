d622a3f61188686f7eef0e8713c583e72f0d8cce
Yash Godara <yash17godara@gmail.com>
2023-12-27 16:38:53 +0530

    styles: viewList Ui and pagination

--- app/(root)/(routes)/products/page.tsx
+++ app/(root)/(routes)/products/page.tsx
@@ -13,7 +13,7 @@ export default async function Page() {
   const products: Product[] = [
     {
       id: "1",
-      productId: "4a213352-836b-4256-9295-d1d837c09aea",
+      productId: "836b-4256-9295-d1d837c09aea",
       name: "NextJs 14 - Template",
       description:
         "This is Template for NextJs 14 Personal Portfolio Website : Styled Using TailwindCss and Headless UI on Top of Shadcn Ui Library and Used Mongodb and Prisma as Database.",
@@ -21,7 +21,7 @@ export default async function Page() {
       ratings: 4,
       discount: 30,
       image: "https://i.imgur.com/OeuV5Lf.png",
-      category: "unknown",
+      category: "y17godara",
       company: "y17godara",
       addedBy: "y17godara",
       createdAt: "2023-12-26T02:35:43.322Z",
@@ -29,10 +29,27 @@ export default async function Page() {
     },
     {
       id: "2",
-      productId: "38cd8bfe-eec7-4ba7-9bec-eca47b64c7b6",
-      name: "ReactJs RoadMap",
-      description: "This is a Roadman for ReactJs in 2024",
-      price: 2,
+      productId: "38cd8bfe-eec7-4ba7-9bec-",
+      name: "NextJs 14 - Template",
+      description:
+        "This is Template for NextJs 14 Personal Portfolio Website : Styled Using TailwindCss and Headless UI on Top of Shadcn Ui Library and Used Mongodb and Prisma as Database.",
+      price: 10,
+      ratings: 5,
+      discount: 10,
+      image: "https://i.imgur.com/OeuV5Lf.png",
+      category: "unknown",
+      company: "y17godara",
+      addedBy: "unknown",
+      createdAt: "2023-12-26T05:04:38.321Z",
+      updatedAt: "2023-12-26T05:02:57.410Z",
+    },
+    {
+      id: "3",
+      productId: "38c48bfe-ca47b64c7b6",
+      name: "NextJs 14 - Template",
+      description:
+        "This is Template for NextJs 14 Personal Portfolio Website : Styled Using TailwindCss and Headless UI on Top of Shadcn Ui Library and Used Mongodb and Prisma as Database.",
+      price: 10,
       ratings: 5,
       discount: 10,
       image: "https://i.imgur.com/OeuV5Lf.png",
--- components/view/viewList.tsx
+++ components/view/viewList.tsx
@@ -1,55 +1,131 @@
 "use client";
-import React from "react";
+import React, { useState } from "react";
 import { type Product } from "@/types/index";
 import Image from "next/image";
 import { FaHeart } from "react-icons/fa";
+import { cn } from "@/lib/utils";
 
-const productSkeleton = () => {
+export function List({
+  id,
+  productId,
+  name,
+  description,
+  price,
+  ratings,
+  discount,
+  image,
+  category,
+  company,
+  addedBy,
+  createdAt,
+  updatedAt,
+}: Product): JSX.Element {
   return (
     <>
-      <div className='flex min-h-32 flex-col gap-4 rounded-md bg-tertiary p-4'></div>
-    </>
-  );
-};
-
-function viewList({ products }: { products: Product[] }): JSX.Element {
-  return (
-    <div className='flex w-full flex-col gap-4 '>
-      {products.map((product: Product) => (
-        <div
-          key={product.productId}
-          className='border-tertiary flex min-h-32 flex-row gap-4 rounded-md border px-2 py-4'
-        >
-          <div className='relative h-48 w-48'>
-            <Image
-              src={product.image}
-              alt={product.name}
-              width={100}
-              height={100}
-              className={"rounded-md"}
-              style={{
-                objectFit: "cover",
-                objectPosition: "center",
-                height: "100%",
-                width: "100%",
-              }}
-            />
+      <li className='border-tertiary flex min-h-32 flex-row gap-4 rounded-md border px-2 py-4'>
+        <div className='relative h-48 w-48 overflow-hidden rounded-md'>
+          <Image
+            src={image}
+            alt={name}
+            width={100}
+            height={100}
+            className={cn(`rounded-md
+                transition-all duration-300 hover:scale-105
+              `)}
+            style={{
+              objectFit: "cover",
+              objectPosition: "center",
+              height: "100%",
+              width: "100%",
+            }}
+          />
+        </div>
+        <div className='flex h-full w-full flex-1 gap-x-4 pt-2 text-start text-xs'>
+          <div>
+            <button
+              title='Favorite'
+              className={"mt-1 hover:text-red-700 focus:text-red-700"}
+            >
+              <FaHeart size={18} />
+            </button>
           </div>
-          <div className='flex h-full w-full flex-1 text-xs'>
+          <div className='flex flex-col gap-2'>
             <div>
-              <FaHeart />
+              <p className='line-clamp-1 text-lg font-bold'>{name}</p>
+              {company === "unknown" ? null : (
+                <p className='line-clamp-1 text-sm underline underline-offset-2'>
+                  {company}
+                </p>
+              )}
             </div>
-            <div>
-              <p className='font-bold'>{product.name}</p>
-              <p className=''>{product.price}</p>
-              <p className=''>{product.description}</p>
-              <p className=''>{product.category}</p>
+            <p className='line-clamp-3 text-base'>{description}</p>
+
+            <div className='flex flex-row gap-2'>
+              <p className='text-lg font-bold text-primary'>${price}</p>
+              <p className='text-sm text-gray-400 line-through'>
+                ${price + discount}
+              </p>
+
+              <p className='text-sm text-gray-400'>{discount}% off</p>
+            </div>
+
+            <div className='flex flex-row gap-2'>
+              <button className='text-sm text-gray-400 underline'>
+                Add to cart
+              </button>
+              <button className='text-sm text-gray-400 underline'>
+                Buy Now
+              </button>
             </div>
           </div>
         </div>
-      ))}
-    </div>
+      </li>
+    </>
   );
 }
 
-export default viewList;
+function ViewList({ products }: { products: Product[] }): JSX.Element {
+  const [currentPage, setCurrentPage] = useState(1);
+  const ITEMS_PER_PAGE = 1; // Adjust this based on your preference
+
+  // Calculate the index range for the current page
+  const startIndex = (currentPage - 1) * ITEMS_PER_PAGE;
+  const endIndex = startIndex + ITEMS_PER_PAGE;
+
+  // Slice the products array to get only the items for the current page
+  const currentProducts = products.slice(startIndex, endIndex);
+
+  // Function to handle pagination
+  const handlePageChange = (newPage: number) => {
+    setCurrentPage(newPage);
+  };
+
+  return (
+    <>
+      <ul className='animate-list flex w-full flex-col gap-4'>
+        {currentProducts.map((product) => (
+          <List key={product.id} {...product} />
+        ))}
+      </ul>
+
+      {/* Pagination controls */}
+      <div>
+        <button
+          onClick={() => handlePageChange(currentPage - 1)}
+          disabled={currentPage === 1}
+        >
+          Previous
+        </button>
+        <span>{currentPage}</span>
+        <button
+          onClick={() => handlePageChange(currentPage + 1)}
+          disabled={endIndex >= products.length}
+        >
+          Next
+        </button>
+      </div>
+    </>
+  );
+}
+
+export default ViewList;
