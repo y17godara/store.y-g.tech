91e6b5a0c0644e839cb327c22478c16cdf11a918
Yash Godara <yash17godara@gmail.com>
2023-12-29 20:42:03 +0530

    styles: added Product Sliders

--- app/(root)/(routes)/page.tsx
+++ app/(root)/(routes)/page.tsx
@@ -2,6 +2,8 @@ import React from "react";
 import HomeCarousel from "../components/Carousel";
 import ProductsSlider from "../components/ProductsSlider";
 import Features from "../components/Features";
+import ProductsSection from "../components/ProductsSection";
+import { title } from "process";
 
 export default async function page() {
   return (
@@ -12,27 +14,139 @@ export default async function page() {
         </div>
 
         <div
-          className='flow-col flex w-full animate-in gap-8'
+          className='flex w-full animate-in flex-col gap-8'
           style={{ "--index": 1 } as React.CSSProperties}
         >
           <Features />
         </div>
 
+        {/* Personalized Products */}
         <div
           className='flow-col flex animate-in gap-2 md:gap-8'
-          style={{ "--index": 1 } as React.CSSProperties}
+          style={{ "--index": 2 } as React.CSSProperties}
         >
-          <div
-            className='flex flex-col items-center justify-center text-wrap rounded-md border border-secondary bg-secondary
+          <div className='flex flex-col gap-4 '>
+            <div
+              className='flex flex-col items-center justify-center text-wrap rounded-md border border-secondary bg-secondary
               p-2
                 text-center transition-all duration-300'
-          >
-            <span className='text-sm font-bold md:text-lg'>
-              Explore Deals on Your Favourite Products
-            </span>
+            >
+              <span className='text-sm font-bold md:text-lg'>
+                Explore Deals on Your Favourite Products
+              </span>
+            </div>
+            <ProductsSlider
+              slides={[
+                {
+                  id: 1,
+                  title: "Product 1",
+                  href: "/",
+                  src: "/og-bg.png",
+                  alt: "Product 1",
+                },
+                {
+                  id: 2,
+                  title: "Product 1",
+                  href: "/",
+                  src: "/og-bg.png",
+                  alt: "Product 1",
+                },
+                {
+                  id: 3,
+                  title: "Product 1",
+                  href: "/",
+                  src: "/og-bg.png",
+                  alt: "Product 1",
+                },
+              ]}
+            />
           </div>
-          <ProductsSlider />
         </div>
+
+        {/* Products */}
+        <ProductsSection
+          slides={[
+            {
+              id: 1,
+              title: "Product 1",
+              href: "/",
+              src: "/og-bg.png",
+              alt: "Product 1",
+            },
+            {
+              id: 2,
+              title: "Product 1",
+              href: "/",
+              src: "/og-bg.png",
+              alt: "Product 1",
+            },
+            {
+              id: 3,
+              title: "Product 1",
+              href: "/",
+              src: "/og-bg.png",
+              alt: "Product 1",
+            },
+          ]}
+          title={"Explore Deals on Your Favourite Products"}
+        />
+
+        {/* Products */}
+        <ProductsSection
+          span='right'
+          slides={[
+            {
+              id: 1,
+              title: "Product 1",
+              href: "/",
+              src: "/og-bg.png",
+              alt: "Product 1",
+            },
+            {
+              id: 2,
+              title: "Product 1",
+              href: "/",
+              src: "/og-bg.png",
+              alt: "Product 1",
+            },
+            {
+              id: 3,
+              title: "Product 1",
+              href: "/",
+              src: "/og-bg.png",
+              alt: "Product 1",
+            },
+          ]}
+          title={"New Arrivals"}
+        />
+
+        {/* View History */}
+        <ProductsSection
+          slides={[
+            {
+              id: 1,
+              title: "Product 1",
+              href: "/",
+              src: "/og-bg.png",
+              alt: "Product 1",
+            },
+            {
+              id: 2,
+              title: "Product 1",
+              href: "/",
+              src: "/og-bg.png",
+              alt: "Product 1",
+            },
+            {
+              id: 3,
+              title: "Product 1",
+              href: "/",
+              src: "/og-bg.png",
+              alt: "Product 1",
+            },
+          ]}
+          alt={"History"}
+        />
       </div>
     </>
   );
--- app/(root)/components/ProductsSlider.tsx
+++ app/(root)/components/ProductsSlider.tsx
@@ -8,8 +8,17 @@ import {
 } from "@/components/shadcn/ui/carousel";
 import { cn } from "@/lib/utils";
 import Image from "next/image";
+import { Link } from "@/components/ui";
 
-const ProductsSlider = () => {
+export type slideProp = {
+  id: number;
+  src: string;
+  alt: string;
+  href: string;
+  title: string;
+};
+
+const ProductsSlider = ({ slides }: { slides?: slideProp[] }) => {
   return (
     <>
       <Carousel
@@ -20,57 +29,36 @@ const ProductsSlider = () => {
         }}
       >
         <CarouselContent className='rounded-md'>
-          <CarouselItem className='relative h-60 w-full overflow-hidden rounded-md md:basis-1/3'>
-            <Image
-              src={"/og-bg.png"}
-              alt={"OG Image"}
-              width={100}
-              height={100}
-              className={cn(`rounded-md
+          {slides?.map((slide: slideProp, i: number) => (
+            <>
+              <CarouselItem
+                key={i}
+                className='relative h-60 w-full overflow-hidden rounded-md md:basis-1/3'
+              >
+                <Link
+                  href={slide.href}
+                  className='relative h-60 w-full overflow-hidden rounded-md '
+                >
+                  <Image
+                    src={slide.src}
+                    alt={slide.alt}
+                    width={100}
+                    height={100}
+                    className={cn(`rounded-md
                 transition-all duration-300
               `)}
-              style={{
-                objectFit: "cover",
-                objectPosition: "center",
-                height: "100%",
-                width: "100%",
-              }}
-            />
-          </CarouselItem>
-          <CarouselItem className='relative h-60 w-full overflow-hidden rounded-md md:basis-1/3'>
-            <Image
-              src={"/og-bg.png"}
-              alt={"OG Image"}
-              width={100}
-              height={100}
-              className={cn(`rounded-md
-                transition-all duration-300
-              `)}
-              style={{
-                objectFit: "cover",
-                objectPosition: "center",
-                height: "100%",
-                width: "100%",
-              }}
-            />
-          </CarouselItem>
-          <CarouselItem className='relative h-60 w-full overflow-hidden rounded-md md:basis-1/3'>
-            <Image
-              src={"/og-bg.png"}
-              alt={"OG Image"}
-              width={100}
-              height={100}
-              className={cn(`rounded-md
-                transition-all duration-300
-              `)}
-              style={{
-                objectFit: "cover",
-                objectPosition: "center",
-                height: "100%",
-                width: "100%",
-              }}
-            />
-          </CarouselItem>
+                    style={{
+                      objectFit: "cover",
+                      objectPosition: "center",
+                      height: "100%",
+                      width: "100%",
+                    }}
+                  />
+                  <p>{slide.title}</p>
+                </Link>
+              </CarouselItem>
+            </>
+          ))}
         </CarouselContent>
       </Carousel>
     </>
