fc3444cd914eb2db9370b360200b61ef3585ea1b
Yash Godara <yash17godara@gmail.com>
2024-01-18 10:16:27 +0530

    styles: Search Not Found Styles

--- app/(root)/(routes)/search/page.tsx
+++ app/(root)/(routes)/search/page.tsx
@@ -1,9 +1,13 @@
+import Image from "next/image";
+import { Redirect } from "next";
+import { Link } from "@/components/ui/index";
+
 import { getSearch } from "@/actions/search";
 import { Grid } from "@/components/view/viewGrid";
-import { Redirect } from "next";
-import { redirect } from "next/dist/server/api-utils";
 
 export const revalidate = 3600;
+
+export const runtime = "edge"; // 'nodejs' (default) | 'edge'
 
 export default async function page({ searchParams }: { searchParams: string }) {
   const { search }: any = searchParams;
@@ -18,7 +22,7 @@ export default async function page({ searchParams }: { searchParams: string }) {
     return (
       <>
         <div className='flex flex-col gap-16 md:gap-24'>
-          <div className='flex animate-in flex-col gap-8'>Not Found</div>
+          <div className='flex animate-in flex-col gap-8'>404 - Not Found</div>
           <div
             className='flex animate-in flex-col gap-8'
             style={{ "--index": 1 } as React.CSSProperties}
@@ -34,20 +38,51 @@ export default async function page({ searchParams }: { searchParams: string }) {
   return (
     <>
       <div className='flex flex-col gap-16 md:gap-24'>
-        <div className='flex animate-in flex-col gap-8'>
-          {!data ? "Searching..." : "Search"}
-        </div>
-
-        <div
-          className='flex animate-in flex-col gap-8'
-          style={{ "--index": 1 } as React.CSSProperties}
-        >
-          {data.map((product: any) => (
-            <div key={product.id}>
-              <Grid {...product} />
+        {data && data.length > 0 ? (
+          <>
+            <div className='flex animate-in flex-col gap-8'>
+              404 - Search Results
             </div>
-          ))}
-        </div>
+            <div
+              className='flex animate-in flex-col gap-8'
+              style={{ "--index": 1 } as React.CSSProperties}
+            >
+              {data.map((product: any) => (
+                <div key={product.id}>
+                  <Grid {...product} />
+                </div>
+              ))}
+            </div>
+          </>
+        ) : (
+          <>
+            <div className='relative flex h-full w-full flex-col items-center justify-center gap-8 text-center '>
+              <div
+                className='flex animate-in flex-col gap-8 text-center'
+                style={{ "--index": 1 } as React.CSSProperties}
+              >
+                OOPS ! No results found for your search
+              </div>
+              <div
+                className='flex animate-in flex-col gap-4 text-center'
+                style={{ "--index": 2 } as React.CSSProperties}
+              >
+                <Image
+                  src='/assets/components/empty-cart-1.svg'
+                  alt='empty cart'
+                  width={100}
+                  quality={50}
+                  priority
+                  className='w-full'
+                  height={100}
+                />
+                <Link href='/' className='hover:underline'>
+                  Explore Products
+                </Link>
+              </div>
+            </div>
+          </>
+        )}
       </div>
     </>
   );
